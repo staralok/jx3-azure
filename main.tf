abdf69a79147f965b409c4110f0d4e6129a58489
staralok <adaipu@gmail.com>
2021-07-06 06:09:21 +0000

    fix: configure cluster repository and project

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cluster" {
-  source                               = "https://github.com/staralok/jx3-azure"
+  source                               = "https://github.com/staralok/jx3-azure.git"
   apex_domain_integration_enabled      = var.apex_domain_integration_enabled
   apex_domain                          = var.apex_domain
   subdomain                            = var.subdomain
